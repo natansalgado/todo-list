5a574971497176ee18e2efe1eb99213cdeca6001
Natan Salgado <natannascimentosalgado@gmail.com>
2023-03-08 19:14:37 -0300

    update 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import { Card } from './components/List'
 import { NewItemScreen } from './components/NewItemScreen'
 import { useState } from 'react'
 
+interface EItem {
+  id: string,
+  content: string
+}
+
 const App = () => {
-  const [todo, setTodo] = useState([{ id: 0, content: 'Wash the dishes' }])
-  const [doing, setDoing] = useState([{ id: 0, content: 'Wash the dishes' }])
-  const [done, setDone] = useState([{ id: 0, content: 'Wash the dishes' }])
+  const [todo, setTodo] = useState<EItem[]>([])
+  const [doing, setDoing] = useState<EItem[]>([])
+  const [done, setDone] = useState<EItem[]>([])
 
   const [newItemScreen, setNewItemScreen] = useState(false)
+
+  const idGenerator = () => {
+    var randomString = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 8; i++) {
+      randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return randomString;
+  }
 
   const openNewItemScreen = () => {
     setNewItemScreen(true)
@@ -17,32 +31,52 @@ const App = () => {
     setNewItemScreen(false)
   }
 
-  const addToTodo = (value: string) => {
-    setTodo(prev => [...prev, { id: todo.length, content: value }])
+  const addNewItem = (value: string, title: string) => {
     closeNewItemScreen()
+    switch (title) {
+      case 'todo':
+        return setTodo(prev => [...prev, { id: idGenerator(), content: value }])
+      case 'doing':
+        return setDoing(prev => [...prev, { id: idGenerator(), content: value }])
+      case 'done':
+        return setDone(prev => [...prev, { id: idGenerator(), content: value }])
+    }
   }
 
-  const addToDoing = (value: string) => {
-    setDoing(prev => [...prev, { id: doing.length, content: value }])
-    closeNewItemScreen()
-  }
-
-  const addToDone = (value: string) => {
-    setDone(prev => [...prev, { id: done.length, content: value }])
-    closeNewItemScreen()
+  const deleteItem = (id: string, title: string) => {
+    switch (title) {
+      case 'todo':
+        const filteredTodo = todo.filter(item => item.id !== id)
+        return setTodo(filteredTodo)
+      case 'doing':
+        const filteredDoing = doing.filter(item => item.id !== id)
+        return setDoing(filteredDoing)
+      case 'done':
+        const filteredDone = done.filter(item => item.id !== id)
+        return setDone(filteredDone)
+    }
   }
 
   return (
-    <div className='w-full h-full py-2 center gap-5 relative flex-wrap'>
-      <Card title="to do" items={todo} addNewItem={openNewItemScreen} />
-      <Card title="doing" items={doing} addNewItem={openNewItemScreen} />
-      <Card title="done" items={done} addNewItem={openNewItemScreen} />
+    <div className='overflow-hidden h-full w-full flex flex-col justify-between items-center'>
+      <div className='personal-container scroll snap-y h-full w-full flex justify-center items-center gap-2 flex-wrap overflow-auto'>
+        <Card title="todo" items={todo} deleteItem={deleteItem} />
+        <Card title="doing" items={doing} deleteItem={deleteItem} />
+        <Card title="done" items={done} deleteItem={deleteItem} />
+      </div>
+      <div className='w-full gradient-bg personal-shadow'>
+        <div className='py-2 px-2 text-center bg-[#00000030]'>
+          <button onClick={openNewItemScreen} className='max-w-sm py-2 w-full gradient-bg rounded-xl personal-shadow'>
+            <p className='text-2xl text-white font-bold uppercase'>
+              new item
+            </p>
+          </button>
+        </div>
+      </div>
       {newItemScreen &&
         <NewItemScreen
           close={closeNewItemScreen}
-          addToTodo={addToTodo}
-          addToDoing={addToDoing}
-          addToDone={addToDone}
+          addNewItem={addNewItem}
         />
       }
     </div>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,43 +1,40 @@
-import { FaPlus } from "react-icons/fa";
+import { FaRegTrashAlt } from 'react-icons/fa'
 
 interface EItems {
-  id: number,
+  id: string,
   content: string
 }
 
 interface ECardProps {
   title: string,
   items?: EItems[],
-  addNewItem: () => void
+  deleteItem: (id: string, title: string) => void
 }
 
-export const Card = ({ title, items, addNewItem }: ECardProps) => {
+export const Card = ({ title, items, deleteItem }: ECardProps) => {
 
   return (
-    <div className='w-full mx-2 max-w-sm h-full max-h-[700px] rounded-md bg-[#00000060] border-[1px] border-[#ffffff70] shadow-lg shadow-black/30 flex flex-col justify-between'>
-      <div className="h-[600px]">
-        <div className="py-2 px-6 font-sans text-2xl font-bold text-[#fff] border-b-[1px] border-b-[#ffffff70] uppercase flex justify-between gap-2 gradient-header">
-          <h1>{title}</h1>
-        </div>
-        <div className="h-full overflow-y-auto">
-          {items &&
-            items.map(item =>
-              <p
-                key={item.id}
-                className="text-xl font-bold text-[#fff] p-2 border-b-[1px] border-b-[#ffffff70] last-of-type:border-none"
-              >
-                {item.content}
-              </p>
-            )
-          }
-        </div>
+    <ul className='list snap-end last-of-type:mb-1 w-full mx-2 max-w-sm h-[89vh] rounded-xl bg-[#00000060] personal-shadow flex flex-col justify-between'>
+      <div className="py-2 px-6 font-sans text-2xl font-bold uppercase flex justify-between gap-2 rounded-t-xl gradient-bg text-white">
+        <h1>{title}</h1>
       </div>
-      <button onClick={addNewItem} className='py-2 w-full flex justify-center items-center gap-2 gradient-header border-t-[1px] border-t-[#ffffff70]'>
-        <p className='text-2xl text-white font-bold uppercase'>
-          new item
-        </p>
-      </button>
-
-    </div>
+      <div className="scroll h-full overflow-y-auto">
+        {items &&
+          items.map(({ id, content }) =>
+            <li key={id} className='flex items-center justify-between border-b-[1px] border-b-[#fff5] last-of-type:border-none'>
+              <p className="text-xl font-bold text-[#fff] p-4">
+                {content}
+              </p>
+              <div className='mx-4'>
+                <button onClick={() => deleteItem(id, title)}>
+                  <FaRegTrashAlt color='white' size={20} />
+                </button>
+              </div>
+            </li>
+          )
+        }
+      </div>
+      <div className="h-2 rounded-b-xl gradient-bg" />
+    </ul>
   )
 }
